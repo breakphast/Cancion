b72339041bb8de4fcca49d45e432f32a3ad81920
Desmond Fitch <desmond.fitch@icloud.com>
2024-02-15 08:49:26 -0500

    Initial Commit

---
+++ Cancion/CancionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CancionApp.swift
+//  Cancion
+//
+//  Created by Desmond Fitch on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CancionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
